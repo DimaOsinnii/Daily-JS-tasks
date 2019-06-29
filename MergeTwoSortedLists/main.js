240526db18bca5066eff8d235e52e9225810cf40
DimaOsinnii <ozerland@gmail.com>
2019-06-29 18:52:21 +0300

    completed task

--- MergeTwoSortedLists/main.js
+++ MergeTwoSortedLists/main.js
@@ -5,6 +5,43 @@ Merge two sorted linked lists and return it as a new list. The new list should b
 
 Input: 1->2->4, 1->3->4
 Output: 1->1->2->3->4->4*/
-function mergeTwoSortedLists() {
+function mergeTwoSortedLists(firstList, secondList, list, node) {
 
+    if(firstList === null) return secondList;
+    if(secondList === null) return  firstList;
+
+    if(firstList && secondList) {
+        if(firstList.val <= secondList.val)  {
+            node = firstList;
+            firstList = node.next;
+        }
+        else {
+            node = secondList;
+            secondList = node.next;
+        }
+    }
+    list = node;
+
+    while(firstList && secondList) {
+        if(firstList.val <= secondList.val) {
+            node.next = firstList;
+            node = firstList;
+            firstList = node.next;
+        }
+        else  {
+            node.next = secondList;
+            node = secondList;
+            secondList = node.next;
+        }
+    }
+
+    if(firstList === null) node.next = secondList;
+    if(secondList === null) node.next = firstList;
+
+    return list;
+}
+
+function ListNode(val, next = null) {
+    this.val = val;
+    this.next = next;
 }
