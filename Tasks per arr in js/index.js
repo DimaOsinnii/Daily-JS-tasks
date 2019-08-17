4adfee1103d9c43b5c5ddfc995b9f08ba8d8488b
DimaOsinnii <ozerland@gmail.com>
2019-08-17 23:26:52 +0300

    rename

--- Tasks per arr in js/index.js
+++ Tasks per arr in js/index.js
@@ -1,6 +1,26 @@
 //Write a function witch convert string type of "my-short-string" to "myShortString"
 function camelize(str) {
-    return str.split('-').map((word, index) => index === 0 ? word: word[0].toUpperCase() + word.slice(1)).join('');
+    return str
+        .split('-')
+        .map((word, index) => index === 0 ? word: word[0].toUpperCase() + word.slice(1))
+        .join('');
 }
 alert(camelize("lol-kekicg-cheburekich"));
+
+//Range filtering with copying into new array
+let arr = [5, 3, 8, 1];
+function filterRange(arr, from, to) {
+    return arr.filter(item => item >= from && item <= to);
+}
+alert(filterRange(arr, 1, 10));
+
+//Range filtering with convert array
+function filterRangeInPlace(arr, from, to) {
+    arr.forEach((item, index) => {
+        if (from <= arr[index] || arr[index] <= to) arr.splice(index, 1);
+    });
+}
+filterRangeInPlace(arr, 1, 4,);
+console.log(arr);
+
 //
